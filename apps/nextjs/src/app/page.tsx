9e1a9240407c175c93e249307f25f2537049c7ef
elevyg <elevyg91@gmail.com>
2024-05-06 18:15:29 -0400

    feat: texts

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -2,9 +2,11 @@ import { api } from "~/trpc/server";
 
 export default async function Home() {
   const data = await api.getLanding();
-  console.log(data);
-  console.log(data.attributes.artist_work_header);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-eggWhite"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-eggWhite">
+      <h2>{data.attributes.offering_header}</h2>
+      <h2>{data.attributes.journal_header}</h2>
+      <h2>{data.attributes.artist_work_header}</h2>
+    </main>
   );
 }
