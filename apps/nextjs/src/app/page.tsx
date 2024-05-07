e69c9d10a9aa6b92031c5e9a6b5b61738c2b5b1a
elevyg <elevyg91@gmail.com>
2024-05-07 10:21:17 -0400

    feat: navbar

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -3,7 +3,7 @@ import { api } from "~/trpc/server";
 export default async function Home() {
   const data = await api.getLanding();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-eggWhite">
+    <main className="min-h-screen bg-eggWhite">
       <h2>{data.attributes.offering_header}</h2>
       <h2>{data.attributes.journal_header}</h2>
       <h2>{data.attributes.artist_work_header}</h2>
--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -4,10 +4,10 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    colors: {},
     extend: {
       colors: {
         eggWhite: "#FFFCF4",
+        matteBlack: "#232323",
       },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
