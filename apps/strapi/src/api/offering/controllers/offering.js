de0736687832af443ba4bcaf1ec706a42740f9cb
elevyg <elevyg91@gmail.com>
2024-05-09 12:43:32 -0400

    feat: change offering controller to work with slug

--- apps/strapi/src/api/offering/controllers/offering.js
+++ apps/strapi/src/api/offering/controllers/offering.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * offering controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::offering.offering');
+module.exports = createCoreController(
+  "api::offering.offering",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const query = {
+        filters: { slug: id },
+        ...ctx.query,
+      };
+
+      const offering = await strapi.entityService.findMany(
+        "api::offering.offering",
+        query
+      );
+
+      const sanitizedEntity = await this.sanitizeOutput(offering);
+
+      return this.transformResponse(sanitizedEntity[0]);
+    },
+  })
+);
