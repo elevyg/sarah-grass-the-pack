f3015bf68e0ebbb40c5e0f76feaf8c4c75987a46
elevyg <elevyg91@gmail.com>
2024-06-10 14:22:37 +0200

    feat: gallery working good for desktop

--- apps/nextjs/src/app/_components/gallery.tsx
+++ apps/nextjs/src/app/_components/gallery.tsx
@@ -7,6 +7,7 @@ import {
   useCallback,
   useImperativeHandle,
   useState,
+  useRef,
 } from "react";
 
 export type Image = {
@@ -27,30 +28,33 @@ const Gallery = forwardRef(
   ({ images }: Props, outerRef: ForwardedRef<GalleryActions>) => {
     const [moveTo, setMoveTo] = useState(0);
 
+    const containerRef = useRef<HTMLDivElement | null>(null);
+
     const onRightArrowClick = useCallback(
       (id: number) => {
         const updatePosition = Math.min(id, images.length - 1);
         setMoveTo(updatePosition);
+
+        const nextImage = images.at(updatePosition);
+        if (!containerRef.current || !nextImage) return;
         window.scrollTo({ behavior: "smooth" });
-        document.getElementById(`gallery-image-${id}`)?.scrollIntoView({
-          block: "center",
-          inline: "end",
-          behavior: "smooth",
-        });
+        containerRef.current.scrollLeft += nextImage.width;
       },
-      [images.length],
+      [images],
     );
 
-    const onLeftArrowClick = useCallback((id: number) => {
-      const updatePosition = Math.max(id, 0);
-      setMoveTo(updatePosition);
-      window.scrollTo({ behavior: "smooth" });
-      document.getElementById(`gallery-image-${id}`)?.scrollIntoView({
-        block: "center",
-        inline: "end",
-        behavior: "smooth",
-      });
-    }, []);
+    const onLeftArrowClick = useCallback(
+      (id: number) => {
+        const updatePosition = Math.max(id, 0);
+        setMoveTo(updatePosition);
+
+        const nextImage = images.at(updatePosition);
+        if (!containerRef.current || !nextImage) return;
+        window.scrollTo({ behavior: "smooth" });
+        containerRef.current.scrollLeft += -nextImage.width;
+      },
+      [images],
+    );
 
     useImperativeHandle(
       outerRef,
@@ -68,12 +72,14 @@ const Gallery = forwardRef(
     );
 
     return (
-      <div className="flex w-full snap-x snap-mandatory scroll-pl-96 overflow-x-hidden scroll-smooth no-scrollbar">
+      <div
+        className="flex w-full snap-x snap-mandatory scroll-pl-96 overflow-x-scroll scroll-smooth no-scrollbar"
+        ref={containerRef}
+      >
         <div className="flex gap-5 px-[300vw] py-5">
-          {images.map((image, index) => (
+          {images.map((image) => (
             <div
               key={image.id}
-              id={`gallery-image-${index.toString()}`}
               className="flex-shrink-0 snap-start snap-always bg-red-300"
             >
               <Image
